d6ffcb5f206e326e94ff5df4f3e11874129f1ebc
Laurvin <github@dbbv.com>
2017-05-18 20:57:07 +0200

    Update BBC_Programmes_-_scroll_to_On_Now.user.js

--- BBC_Programmes_-_scroll_to_On_Now.user.js
+++ BBC_Programmes_-_scroll_to_On_Now.user.js
@@ -1,12 +1,15 @@
 // ==UserScript==
-// @name        BBC Programmes - scroll to On Now
-// @icon        http://i.imgur.com/XYzKXzK.png
-// @namespace   DB
-// @version     0.2
-// @description After loading the page scrolls the page to the program currently on.
-// @include     http://www.bbc.co.uk/*/programmes/schedules/*
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// @grant       none
+// @name BBC Programmes - scroll to On Now
+// @namespace BBC Programmes - scroll to On Now
+// @author Laurvin
+// @description Scrolls the BBC TV Schedule pages to the programme currently on.
+// @version 0.2
+// @icon http://i.imgur.com/XYzKXzK.png
+// @downloadURL https://github.com/Laurvin/BBC-Programmes---scroll-to-On-Now/raw/master/BBC_Programmes_-_scroll_to_On_Now.user.js
+// @include http://www.bbc.co.uk/*/programmes/schedules/*
+// @require http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @grant none
+// @run-at document-idle
 // ==/UserScript==
 
 this.$ = this.jQuery = jQuery.noConflict(true);
