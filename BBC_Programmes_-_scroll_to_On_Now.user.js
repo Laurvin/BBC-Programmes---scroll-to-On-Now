0d76abc19466aca7822d64393dcf6693028a648b
Laurvin <github@dbbv.com>
2017-11-26 00:27:46 +0100

    Update BBC_Programmes_-_scroll_to_On_Now.user.js

--- BBC_Programmes_-_scroll_to_On_Now.user.js
+++ BBC_Programmes_-_scroll_to_On_Now.user.js
@@ -3,20 +3,21 @@
 // @namespace BBC Programmes - scroll to On Now
 // @author Laurvin
 // @description Scrolls the BBC TV Schedule pages to the programme currently on.
-// @version 0.2
+// @version 0.3
 // @icon http://i.imgur.com/XYzKXzK.png
 // @downloadURL https://github.com/Laurvin/BBC-Programmes---scroll-to-On-Now/raw/master/BBC_Programmes_-_scroll_to_On_Now.user.js
-// @include http://www.bbc.co.uk/*/programmes/schedules/*
+// @include https://www.bbc.co.uk/schedules/*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @grant none
 // @run-at document-idle
 // ==/UserScript==
 
 this.$ = this.jQuery = jQuery.noConflict(true);
+
 $(document).ready(function ()
 {
-  $('html, body').animate(
+    $('html, body').animate(
     {
-        scrollTop: $("#on-now").offset().top
+        scrollTop: $(".broadcast__live").offset().top
     }, 500);
 });
